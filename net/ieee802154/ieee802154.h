2f0a179e9e155342999d1e3680a223435b67be69
Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
2011-12-19 19:33:49 +0300

    ieee802154: remove ieee802154 policy from globals
    Move ieee802154 netlink policy array definition inside ieee802154 layer.
    
    Signed-off-by: Alexander Smirnov <alex.bluesman.smirnov@gmail.com>

--- net/ieee802154/ieee802154.h
+++ net/ieee802154/ieee802154.h
@@ -21,6 +21,8 @@
 int __init ieee802154_nl_init(void);
 void __exit ieee802154_nl_exit(void);
 
+extern const struct nla_policy ieee802154_policy[];
+
 #define IEEE802154_OP(_cmd, _func)			\
 	{						\
 		.cmd	= _cmd,				\
